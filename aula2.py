ac52c7689a016ebec5d50e41fd516daa0b53ffe5
Danielefagundes <daniele.afagundes@gmail.com>
2024-08-09 15:16:13 -0300

    Aula2.py
    Exercícios de aula 5 PYTHON.

---
+++ aula2.py
@@ -0,0 +1,73 @@
+# Exercícios: 
+
+# 1 Imprima uma mensagem de boas-vindas na tela.
+# 2 Declare uma variável booleana verdadeiro com o valor True e imprima seu tipo
+# 3 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+# 4 Imprima o resultado da divisão de dois números inteiros de sua escolha.
+# 5 Imprima o resultado da subtração de dois números inteiros de sua escolha
+# 6 Imprima o resultado da divisão inteira de dois números inteiros de sua escolha.
+# 7 Imprima o resultado da multiplicação de 4 números decimais de sua escolha.
+# 8 Declare uma variável numero e atribua um número inteiro. Em seguida, imprima o dobro desse número.
+# 9 Crie uma calculadora de soma com as ferramentas que vc já conhece(Use apenas input, print e variavel).
+# 10 Crie um sistema de cadastro com as estruturas que vc já conhece(Use apenas input, print e variavel).
+
+
+# Atividade 1
+# print("Boas - vindas")
+
+# Atividade 2
+# booleano_1 = True
+# print(booleano_1)
+
+# Atividade 3
+
+# mult = 1.5*6.8
+# print(mult)
+
+# #Atividade 4
+# div = 55/10
+# print(div)
+
+# Atividade 5
+# sub = 10-5
+# print(sub)
+
+# Atividade 6
+
+# div2 = 25//5
+# print(div2)
+
+#Atividade 7
+# mult2 = 4.5*2.8*6.6*8.7
+# print(mult2)
+
+# Atividade 8
+
+# var_1 = 10
+# mult3 = 10*2
+# print(mult3)
+
+
+# Atividade 9
+
+# calc = int(input("Digite um número: "))
+# calc2 = int(input("Digite um número 2: "))
+# soma1 = calc + calc2
+# print(soma1)
+
+# Atividade 10
+
+# nome = input("Digite seu nome completo: ")
+# end = input("Digite seu endereço: ")
+# doc1 = input("Digite seu CPF: ")
+# print("Cadastro realizado")
+
+
+
+
+
+
+
+
+
+
